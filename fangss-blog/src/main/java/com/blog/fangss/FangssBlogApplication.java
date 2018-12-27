59a7e31462a3bf77049982edc7796c4afdf69869
fangshanshan <643915247@qq.com>
2018-12-27 14:35:45 +0800

    项目重构jar版本

---
+++ fangss-blog/src/main/java/com/blog/fangss/FangssBlogApplication.java
@@ -0,0 +1,14 @@
+package com.blog.fangss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FangssBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FangssBlogApplication.class, args);
+    }
+
+}
+
