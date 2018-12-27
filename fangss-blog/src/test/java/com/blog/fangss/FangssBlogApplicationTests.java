6a4e2b6c20b121d9cb7bc21182208040e62922bc
fangshanshan <643915247@qq.com>
2018-12-27 12:32:22 +0800

    项目初始化提交

---
+++ fangss-blog/src/test/java/com/blog/fangss/FangssBlogApplicationTests.java
@@ -0,0 +1,17 @@
+package com.blog.fangss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FangssBlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
---
+++ fangss-utils/src/main/java/com/utils/fangss/FangssUtilsApplication.java
@@ -0,0 +1,14 @@
+package com.utils.fangss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FangssUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FangssUtilsApplication.class, args);
+    }
+
+}
+
