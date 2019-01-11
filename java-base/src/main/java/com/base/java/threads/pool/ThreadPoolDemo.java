51b6b7a5eb007628ced1c2a43a519f2c11ae16aa
fangshanshan <643915247@qq.com>
2019-01-11 18:01:55 +0800

    个人博客 模版

---
+++ java-base/src/main/java/com/base/java/threads/pool/ThreadPoolDemo.java
@@ -0,0 +1,12 @@
+package com.base.java.threads.pool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor;
+
+        System.out.println(Integer.SIZE);
+        System.out.println();
+    }
+}
